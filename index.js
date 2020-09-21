e34614497cf07ea55b7c1892e908dfb7b0341a0d
Darren O'Flaherty <darren@reburas-air.home>
2020-09-21 23:08:34 +0100

    node-weather

---
+++ index.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const argv = require('yargs').argv;
+
+let apiKey = '471bd228fd355f5e1f771cff81101d77';
+let city = argv.c || 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+  }
+});
+
+
